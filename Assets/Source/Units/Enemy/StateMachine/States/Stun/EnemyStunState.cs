1653fcfc92dae723ac43d963dd78951f0991b47d
igoryava <126491702+igoryava@users.noreply.github.com>
2025-09-09 20:58:15 +0700

    added stun anim

--- Assets/Source/Units/Enemy/StateMachine/States/Stun/EnemyStunState.cs
+++ Assets/Source/Units/Enemy/StateMachine/States/Stun/EnemyStunState.cs
@@ -6,17 +6,23 @@ using UnityEngine.AI;
 
 public class EnemyStunState : State
 {
+    [SerializeField] private Animator _animator;
+    [SerializeField] private string _boolName;
     [SerializeField] private NavMeshAgent _agent;
     
     public override void Enter()
     {
         CanChanged = false;
         _agent.isStopped = true;
+        Debug.Log("Stun");
+        _animator.SetBool(_boolName, true);
     }
 
     public void UnStun()
     {
         CanChanged = true;
+        Debug.Log("UnStun");
+        _animator.SetBool(_boolName, false);
         _agent.isStopped = false;
     }
 }
