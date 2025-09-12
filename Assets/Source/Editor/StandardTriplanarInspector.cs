12e6a0c2326e5da768a7543fd05ee386a504fc26
Marat <nmg1033@gmail.com>
2025-09-12 18:29:30 +0300

    added first iteration on new arena

---
+++ Assets/Source/Editor/StandardTriplanarInspector.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEditor;
+
+public class StandardTriplanarInspector : ShaderGUI
+{
+    static class Styles
+    {
+        static public readonly GUIContent albedo = new GUIContent("Albedo", "Albedo (RGB)");
+        static public readonly GUIContent emission = new GUIContent("Emission", "Emission (RGB)");
+        static public readonly GUIContent normalMap = new GUIContent("Normal Map", "Normal Map");
+        static public readonly GUIContent occlusion = new GUIContent("Occlusion", "Occlusion (G)");
+    }
+
+    bool _initialized;
+
+    public override void OnGUI(MaterialEditor editor, MaterialProperty[] props)
+    {
+        EditorGUI.BeginChangeCheck();
+
+        editor.TexturePropertySingleLine(
+            Styles.albedo, FindProperty("_MainTex", props), FindProperty("_Color", props)
+        );
+
+        editor.TexturePropertySingleLine(
+            Styles.emission, FindProperty("_EmissionTex", props), FindProperty("_EmissionColor", props)
+        );
+
+        editor.ShaderProperty(FindProperty("_Metallic", props), "Metallic");
+        editor.ShaderProperty(FindProperty("_Glossiness", props), "Smoothness");
+
+        var normal = FindProperty("_BumpMap", props);
+        editor.TexturePropertySingleLine(
+            Styles.normalMap, normal,
+            normal.textureValue ? FindProperty("_BumpScale", props) : null
+        );
+
+        var occ = FindProperty("_OcclusionMap", props);
+        editor.TexturePropertySingleLine(
+            Styles.occlusion, occ,
+            occ.textureValue ? FindProperty("_OcclusionStrength", props) : null
+        );
+
+        editor.ShaderProperty(FindProperty("_MapScale", props), "Texture Scale");
+        editor.ShaderProperty(FindProperty("_EmissionMapScale", props), "Emission Texture Scale");
+
+        if (EditorGUI.EndChangeCheck() || !_initialized)
+            foreach (Material m in editor.targets)
+                SetMaterialKeywords(m);
+
+        _initialized = true;
+    }
+
+    static void SetMaterialKeywords(Material material)
+    {
+        SetKeyword(material, "_NORMALMAP", material.GetTexture("_BumpMap"));
+        SetKeyword(material, "_OCCLUSIONMAP", material.GetTexture("_OcclusionMap"));
+    }
+
+    static void SetKeyword(Material m, string keyword, bool state)
+    {
+        if (state)
+            m.EnableKeyword(keyword);
+        else
+            m.DisableKeyword(keyword);
+    }
+}
